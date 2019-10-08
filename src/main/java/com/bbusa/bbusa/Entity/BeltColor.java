83809feb3b79c2dc6758c9f70755da8f56b74f35
Mark Kang <sk531@duke.edu>
2019-10-08 19:00:47 -0400

    fixed belt color bug

--- src/main/java/com/bbusa/bbusa/Entity/BeltColor.java
+++ src/main/java/com/bbusa/bbusa/Entity/BeltColor.java
@@ -2,8 +2,7 @@ package com.bbusa.bbusa.Entity;
 
 public enum BeltColor {
     WHITE(0), ORANGE(1), YELLOW(2), GREEN(3), PURPLE(4), BLUE(5),
-    BROWN(6), RED(7), REDDOUBLESTRIPE(8), REDBLACK(9), BLACKFIRSTDEGREE(10),
-    BLACKSECONDDEGREE(11), BLACKTHIRDDEGREE(12);
+    BROWN(6), RED(7), REDDOUBLESTRIPE(8), REDBLACK(9), BLACK(10);
 
     private int value;
     BeltColor(int index){
