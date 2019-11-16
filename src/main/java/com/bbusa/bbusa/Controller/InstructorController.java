c4e26de04e8396b0f2c63440067547e2fca5f38f
Frank Hu <frankhu19@gmail.com>
2019-11-16 15:12:38 -0500

    added repository for attends

--- src/main/java/com/bbusa/bbusa/Controller/InstructorController.java
+++ src/main/java/com/bbusa/bbusa/Controller/InstructorController.java
@@ -1,12 +1,39 @@
 package com.bbusa.bbusa.Controller;
 
+import com.bbusa.bbusa.APIResponse.ClassListAPIResponse;
+import com.bbusa.bbusa.Entity.ClassesEntity;
+import com.bbusa.bbusa.Entity.InstructorEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class InstructorController {
 
     //get profile
     //get list of classes
+
+    @GetMapping("/getClassList")
+    public List<ClassListAPIResponse> getClassList(HttpServletResponse httpServletResponse, @RequestParam String student_email){
+        addCrossOrigins(httpServletResponse);
+        List<ClassListAPIResponse> classList = new ArrayList<>();
+        List<ClassesEntity> classesEntities =  classesRepository.getListOfClassesOfStudent(student_email);
+
+        for(ClassesEntity classesEntity: classesEntities){
+            ClassListAPIResponse classListAPIResponse = new ClassListAPIResponse();
+            int CID = classesEntity.getCID();
+            List<InstructorEntity> instructorEntities = instructorRepository.getClassProfile(CID);
+            classListAPIResponse.setClassesEntity(classesEntity);
+            classListAPIResponse.setInstructorEntities(instructorEntities);
+            classList.add(classListAPIResponse);
+        }
+
+        return classList;
+    }
     //add instructor
     //add student and parent
 
--- src/main/java/com/bbusa/bbusa/Repository/AttendsRepository.java
+++ src/main/java/com/bbusa/bbusa/Repository/AttendsRepository.java
@@ -3,6 +3,7 @@ package com.bbusa.bbusa.Repository;
 import com.bbusa.bbusa.Entity.AttendsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,11 @@ import java.util.List;
 @Repository
 public interface AttendsRepository extends JpaRepository<AttendsEntity, String> {
 
-    @Query(value = "SELECT * FROM innodb.Attends" , nativeQuery = true)
-    List<AttendsEntity> getAllAttends();
+    @Query(value = "Select attendDate, attendance from innodb.Attends as a Join innodb.Student as s on a.sid = s.SID" +
+            "where a.SID = :student_email and a.CID = :class_id;" , nativeQuery = true)
+    List<AttendsEntity> getClassAttends(@Param("student_email") String student_email, @Param("class_id") int class_id );
+
+    @Query(value = "Select SID, CID, attendDate, attendance from innodb.Attends as a Join innodb.Student as s on a.sid = s.SID" +
+            "where s.student_email =  = :student_email;" , nativeQuery = true)
+    List<AttendsEntity> getAllAttends(@Param("student_email") String student_email);
 }
