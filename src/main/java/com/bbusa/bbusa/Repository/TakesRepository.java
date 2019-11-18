e9deb3bdd3e97aa4bc6121021309c0c2976710e8
Tiffany Mei <tianxue.mei@duke.edu>
2019-11-18 18:21:21 -0500

    TakesRepo and TeachesRepo

--- src/main/java/com/bbusa/bbusa/Repository/TakesRepository.java
+++ src/main/java/com/bbusa/bbusa/Repository/TakesRepository.java
@@ -5,5 +5,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TakesRepository extends JpaRepository<TakesEntity, String> {
-
+    @Query(value =
+            "Select a.SID, a.CID, start_date " +
+            "FROM Takes as a, Student as s " +
+            "WHERE a.SID = s.SID", nativeQuery = true)
+    List<AttendsEntity> getStudentTakes(@Param("SID") int SID);
 }
--- src/main/java/com/bbusa/bbusa/Repository/TeachesRepository.java
+++ src/main/java/com/bbusa/bbusa/Repository/TeachesRepository.java
@@ -6,5 +6,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeachesRepository extends JpaRepository<TeachesEntity, String> {
-
+    @Query(value =
+            "Select a.IID, a.CID, start_date " +
+                    "FROM Teaches as a, Instructor as i " +
+                    "WHERE a.IID = i.IID", nativeQuery = true)
+    List<AttendsEntity> getInstructorTeaches(@Param("IID") int IID);
 }
