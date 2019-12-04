acaa09cc32c8dbceaec7f9dce2e7386a825c58b8
Mark Kang <sk531@duke.edu>
2019-12-03 21:15:13 -0500

    able to get list of classes for instructor

--- src/main/java/com/bbusa/bbusa/Controller/InstructorController.java
+++ src/main/java/com/bbusa/bbusa/Controller/InstructorController.java
@@ -3,6 +3,10 @@ package com.bbusa.bbusa.Controller;
 import com.bbusa.bbusa.APIResponse.ClassListAPIResponse;
 import com.bbusa.bbusa.Entity.ClassesEntity;
 import com.bbusa.bbusa.Entity.InstructorEntity;
+import com.bbusa.bbusa.Repository.ClassesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +20,23 @@ import java.util.List;
 @RestController
 public class InstructorController {
 
-    //get profile
-    //get list of classes
+    @Value("${cors.host}")
+    private static String corsHost;
 
-    //add instructor
-    //add student and parent
+    private static final String INSTRUCTOR = "instructor";
+
+    @Autowired
+    private ClassesRepository classesRepository;
+
+    @GetMapping("/instructorClasses")
+    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+    public List<ClassesEntity> getInstructorClasses(@RequestParam(value = INSTRUCTOR) String instructor_email){
+        List<ClassesEntity> classesEntities = classesRepository.getListOfClassesOfInstructor(instructor_email);
+        return classesEntities;
+    }
+
+    //TODO: get profile
+    //TODO: add instructor
+    //TODO: add student and parent
 
 }
--- src/main/java/com/bbusa/bbusa/Repository/ClassesRepository.java
+++ src/main/java/com/bbusa/bbusa/Repository/ClassesRepository.java
@@ -14,4 +14,10 @@ public interface ClassesRepository extends JpaRepository<ClassesEntity, String> 
     @Query(value = "SELECT t.CID, start_time, end_time, target_start_age, target_older_age, description " +
             "FROM Classes as c, Student as s, Takes as t WHERE s.student_email = :student_email AND t.SID = s.SID AND c.CID = t.CID", nativeQuery = true)
     List<ClassesEntity> getListOfClassesOfStudent(@Param("student_email") String student_email);
+
+    @Query(value = "SELECT t.CID, start_time, end_time, target_start_age, target_older_age, description " +
+            "FROM Classes as c, Instructor as i, Teaches as t WHERE i.instructor_email = :instructor_email AND t.IID = i.IID AND c.CID = t.CID", nativeQuery = true)
+    List<ClassesEntity> getListOfClassesOfInstructor(@Param("instructor_email") String instructor_email);
+
+
 }
